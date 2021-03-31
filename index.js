21d0b7ce91dab53ea815ae8ab766d7f3defdc22e
jiangWeixian <jiangweixian1994@gmail.com>
2021-03-31 08:36:17 +0800

    🎉 is create work?

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const core = require('@actions/core');
 const wait = require('./wait');
+const github = require('@actions/github')
+const fs = require('fs')
 
+const octokit = github.getOctokit(token);
 
 // most @actions toolkit packages have async methods
 async function run() {
@@ -11,6 +14,15 @@ async function run() {
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
     core.info((new Date()).toTimeString());
+    core.info(JSON.stringify(process.env))
+    const results = await octokit.repos.listStargazersForRepo({
+      owner: 'jiangweixian',
+      repo: 'templates'
+    })
+
+    fs.writeFileSync('./results.json', JSON.stringify(results))
+
+    core.info(fs.readFileSync('./results.json').toString('utf-8'))
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
